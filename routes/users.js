b6147de3f7ca5bc557e43ae36ed40d40bb365db5
Levi Winans <levieiko@gmail.com>
2019-05-12 10:37:59 -0700

    map up and running

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-const User = require('../models/User')
+const User = require('../models/User');
 
 router.get('/', (req, res) => {
   return res.json({data: 'received a GET"'})
 });
 
-router.post('/', async(req, res) => {
-  try{
-    console.log(req.body)
+router.post('/', async (req, res) => {
+  try {
     const user = await User.create(req.body)
-    // const User = await User.find({})
-    console.log("finsihed creating")
-    console.log(user)
-    res.json({data:user, success:true})
+    res.json({user})
   } catch(err) {
-    console.log(err)
+    res.json({err})
   }
-  
 });
 
 router.put('/', (req, res) => {
@@ -27,11 +22,11 @@ router.put('/', (req, res) => {
 
 router.delete('/', (req, res) => {
   return res.json({ data: "Received a DELETE"})
-})
+});
 
 router.post('/login', async (req, res) => {
   try {
-    const foundUser = await User.find({username: req.body.name})
+    const foundUser = await User.find({username: req.body.username})
     res.json({
       user: foundUser,
       success: true
@@ -41,6 +36,13 @@ router.post('/login', async (req, res) => {
   }
 });
 
-
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    res.json({user})
+  } catch(err) {
+    res.json({err})
+  }
+});
 
 module.exports = router;
